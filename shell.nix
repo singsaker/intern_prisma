ec86ef860fb813f0bea698f71e9669a341aed9e5
Daniel Olsen <daniel.olsen99@gmail.com>
2021-10-11 16:55:04 +0200

    Wrangling nodejs is a challenge

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
-{ pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import <nixos-unstable> {}}:
 
 pkgs.mkShell {
-  nativeBuildInputs = [ pkgs.docker-compose pkgs.nodejs-12_x ];
+  nativeBuildInputs = [
+    pkgs.docker-compose
+    pkgs.nodejs-12_x
+    pkgs.nodePackages.prisma
+    pkgs.nodePackages.node2nix
+  ];
 }
